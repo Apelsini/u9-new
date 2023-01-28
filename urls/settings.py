25c01958b687d857054eb8e8a0361c1093de8438
Apelsini <apelsin652@gmail.com>
2023-01-28 21:48:35 +0300

    siteid=1

--- urls/settings.py
+++ urls/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     'matomo',  #matomo analytics https://pypi.org/project/django-matomo/
 ]
 
-MATOMO_SITE_ID = 2
+MATOMO_SITE_ID = 1
 MATOMO_URL = 'http://apps.u9.by/Matomo/'
 
 MIDDLEWARE = [

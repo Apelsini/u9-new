4a78f19b5ead6006084163c60955bced7685d139
Apelsini <apelsin652@gmail.com>
2023-02-01 22:39:20 +0300

    Update views.py

--- polls/views.py
+++ polls/views.py
@@ -35,7 +35,7 @@ class IndexView(generic.ListView):    #Class-Based View
     def get_queryset(self):
         filter_url = self.request.GET.get('filter_url', '')
         filter_author = self.request.GET.get('filter_author', self.request.user)
-        filter_datefrom = self.request.GET.get('filter_datefrom', timezone.datetime.date(2022, 1, 30))
+        filter_datefrom = self.request.GET.get('filter_datefrom', timezone.now())
         filter_dateto = self.request.GET.get('filter_dateto', timezone.now())
         order = self.request.GET.get('orderby', '-create_date')
         new_context = Urlentry.objects.filter(
@@ -49,7 +49,7 @@ class IndexView(generic.ListView):    #Class-Based View
         context = super(IndexView, self).get_context_data(**kwargs)
         context['filter_url'] = self.request.GET.get('filter_url', '')
         context['filter_author'] = self.request.GET.get('filter_author', self.request.user)
-        context['filter_datefrom'] = self.request.GET.get('filter_datefrom', timezone.datetime.date(2022, 1, 30))
+        context['filter_datefrom'] = self.request.GET.get('filter_datefrom', timezone.now())
         context['filter_dateto'] = self.request.GET.get('filter_dateto', timezone.now())
         context['orderby'] = self.request.GET.get('orderby', '-create_date')
         return context

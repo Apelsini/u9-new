c129c3b5144d8f6be3be8d6b27fa7e25040be835
Apelsini <apelsin652@gmail.com>
2023-03-02 17:49:15 +0300

    Update views.py

--- polls/views.py
+++ polls/views.py
@@ -41,7 +41,7 @@ class IndexView(generic.ListView):    #Class-Based View
         order = self.request.GET.get('orderby', '-create_date')
         page = self.request.GET.get('page', 1)
         new_context = Urlentry.objects.filter(
-            #author=filter_author,
+            author=filter_author,
             url_text__contains=filter_url,   #__contains lookup
             #create_date__gte=filter_datefrom,
             #create_date__lte=filter_dateto,
@@ -90,7 +90,6 @@ def detailview_urlentry(request, pk):
     leads_list = [i for i in Leads.objects.all()]
     return      render(request, 'polls/results.html',{
     })
-
 
 class ResultsView(generic.DetailView):
     model = Urlentry

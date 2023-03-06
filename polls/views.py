c7f4193b0385b528a450d97d52f9e2429320f326
Apelsini <apelsin652@gmail.com>
2023-03-06 14:07:55 +0300

    Update views.py

--- polls/views.py
+++ polls/views.py
@@ -36,8 +36,8 @@ class IndexView(generic.ListView):    #Class-Based View
     def get_queryset(self):
         filter_url = self.request.GET.get('filter_url', '')
         filter_author = self.request.GET.get('filter_author', self.request.user) #User.objects.get(username=self.request.GET.get('filter_author', self.request.user))
-        filter_datefrom = datetime.strptime(datetime.strftime(self.request.GET.get('filter_datefrom', timezone.now().replace(year=2022)),"%Y-%m-%d %H:%M"),"%Y-%m-%d %H:%M")  #"%d/%m/%Y %H:%M %p"
-        filter_dateto = datetime.strptime(datetime.strftime(self.request.GET.get('filter_dateto', timezone.now()),"%Y-%m-%d %H:%M"), "%Y-%m-%d %H:%M") # timezone.now().strftime("%Y-%m-%d %H:%M")
+        filter_datefrom = datetime.strptime(datetime.strptime(self.request.GET.get('filter_datefrom', timezone.now().replace(year=2022)),"%Y-%m-%d %H:%M"),"%Y-%m-%d %H:%M")  #"%d/%m/%Y %H:%M %p"
+        filter_dateto = datetime.strptime(datetime.strptime(self.request.GET.get('filter_dateto', timezone.now()),"%Y-%m-%d %H:%M"), "%Y-%m-%d %H:%M") # timezone.now().strftime("%Y-%m-%d %H:%M")
         order = self.request.GET.get('orderby', '-create_date')
         page = self.request.GET.get('page', 1)
         if self.request.user.is_superuser:    #is superuser

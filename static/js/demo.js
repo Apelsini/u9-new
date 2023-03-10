5c0442c2e291ca22d59392d7713de2592035bafc
Apelsini <apelsin652@gmail.com>
2023-03-10 16:04:02 +0300

    bootstrap3 datetime picker

---
+++ static/js/demo.js
@@ -0,0 +1,39 @@
+(function($){
+    $(function(){
+        $('#filter_datefrom').datetimepicker({
+            "allowInputToggle": true,
+            "showClose": true,
+            "showClear": true,
+            "showTodayButton": true,
+            "format": "YYYY-MM-DD HH:MM",
+        });
+        $('#filter_dateto').datetimepicker({
+            "allowInputToggle": true,
+            "showClose": true,
+            "showClear": true,
+            "showTodayButton": true,
+            "format": "YYYY-MM-DD HH:MM",
+        });
+        $('#id_2').datetimepicker({
+            "allowInputToggle": true,
+            "showClose": true,
+            "showClear": true,
+            "showTodayButton": true,
+            "format": "hh:mm:ss A",
+        });
+        $('#id_3').datetimepicker({
+            "allowInputToggle": true,
+            "showClose": true,
+            "showClear": true,
+            "showTodayButton": true,
+            "format": "HH:mm:ss",
+        });
+        $('#id_4').datetimepicker({
+            "allowInputToggle": true,
+            "showClose": true,
+            "showClear": true,
+            "showTodayButton": true,
+            "format": "MM/DD/YYYY",
+        });
+    });
+})(jQuery);

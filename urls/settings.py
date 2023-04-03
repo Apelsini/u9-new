2db1241da726eeaa5866e965c054f14ecd8ff8b7
Apelsini <apelsin652@gmail.com>
2023-04-03 11:20:33 +0300

    Update settings.py

--- urls/settings.py
+++ urls/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import json
 from django.apps import apps
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.

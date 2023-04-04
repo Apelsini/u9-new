64438427bb59a1e55da61e342adf4a4850cfd575
Apelsini <apelsin652@gmail.com>
2023-04-04 10:11:46 +0300

    Update urls.py

--- authentication/urls.py
+++ authentication/urls.py
@@ -7,9 +7,7 @@ urlpatterns = [
     path('register/', views.register_page, name="reg"),
     path('login/', views.login_page, name='login'),
     path('reset_password/', view=auth_views.PasswordResetView.as_view(template_name='authentication/password_reset.html'), name='reset_password'),
-    path('reset_password_sent/',
-         view=auth_views.PasswordResetDoneView.as_view(template_name='authentication/reset_password_sent.html'),
-         name='reset_password'),
+    
     path('logout/', views.logout_page, name='logout'),
     path('users/', views.UsersView.as_view(), name='users'),
     path('profile<int:pk>/', views.profile_page, name='profile'),

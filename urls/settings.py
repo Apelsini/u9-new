d9f5c835a83abde747440909ec21a413212c4b0f
Apelsini <apelsin652@gmail.com>
2023-04-04 16:28:48 +0300

    Update settings.py

--- urls/settings.py
+++ urls/settings.py
@@ -165,8 +165,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # stmp email configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'mail.u9.by'
-EMAIL_PORT = 26
-EMAIL_USE_TLS = False
-#EMAIL_USE_SSL = True
+EMAIL_PORT = 465
+#EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', email_secret)
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', emailpass_secret)

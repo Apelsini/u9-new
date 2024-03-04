9670a743c7a89756029236cd3b8c857a7b620376
Apelsini <apelsin652@gmail.com>
2024-03-04 12:23:43 +0300

    messages to Profile details

--- authentication/views.py
+++ authentication/views.py
@@ -57,25 +57,13 @@ def profile_page(request, pk):  #shows profile details
     else:
         form = UserNotificationForm()
         form.fields['email1'].initial = profile.email1
-        if profile.email1cb:
-            form.fields['email1cb'].initial = 'true'
-        else:
-            form.fields['email1cb'].initial = 'false'
+        form.fields['email1cb'].initial = profile.email1cb
         form.fields['email2'].initial = profile.email2
-        if profile.email2cb:
-            form.fields['email2cb'].initial = 'true'
-        else:
-            form.fields['email2cb'].initial = 'false'
+        form.fields['email2cb'].initial = profile.email2cb
         form.fields['telegram1'].initial = profile.telegram1
-        if profile.telegram1cb:
-            form.fields['telegram1cb'].initial = 'true'
-        else:
-            form.fields['telegram1cb'].initial = 'false'
+        form.fields['telegram1cb'].initial = profile.telegram1cb
         form.fields['telegram2'].initial = profile.telegram2
-        if profile.telegram2cb:
-            form.fields['telegram2cb'].initial = 'true'
-        else:
-            form.fields['telegram2cb'].initial = 'false'
+        form.fields['telegram2cb'].initial = profile.telegram2cb
     return render(request, 'authentication/profile.html', context={
         'profile': Profile.objects.get(pk=pk),
         'group':group,

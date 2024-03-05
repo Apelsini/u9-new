3d30a01d0c577937f4b8880bba5058572644890d
Apelsini <apelsin652@gmail.com>
2024-03-05 14:11:26 +0300

    notifier.py

--- notify.py
+++ notify.py
@@ -28,7 +28,7 @@ def send_telegram_message(chat_id, token, message):
     url = f"https://api.telegram.org/bot{token}/sendMessage"
     data = {
         "chat_id": chat_id,
-        "text": message
+        "text": message,
         "parse_mode" : "HTML"
     }
     response = requests.post(url, data=data)

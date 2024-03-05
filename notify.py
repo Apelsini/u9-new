fd7519890e8489beeaa3c1775abfb53641411ed0
Apelsini <apelsin652@gmail.com>
2024-03-05 15:04:01 +0300

    notify - telegram1chat_id added

--- notify.py
+++ notify.py
@@ -25,14 +25,9 @@ def isValid(email):
       return False
 
 def send_telegram_message(chat_id, token, message):
-    url = f"https://api.telegram.org/bot{token}/sendMessage"
-    data = {
-        "chat_id": chat_id,
-        "text": message,
-        "parse_mode" : "HTML"
-    }
-    response = requests.post(url, data=data)
-    return response.json()
+    url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}&parse_mode=HTML"
+    response = requests.get(url).json()
+    return response
 
 def send_email_notification(email, message, subject):
     subject = subject

0f30ef8dcf4f2ca94e70e75128e2e81ebe7fccfd
Apelsini <apelsin652@gmail.com>
2024-03-27 16:54:06 +0300

    results - adding location

--- polls/views.py
+++ polls/views.py
@@ -204,7 +204,7 @@ class ResultsView(generic.DetailView):
         # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
         objj = Leads.objects.all().filter(
-            urlentry=self.object)
+            urlentry_pk=self.object.pk)
         # Add in additional details
         country_codes_dict = {}
         if objj:

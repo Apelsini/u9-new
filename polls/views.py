290eed01c5d4d135889b4dc2ddd12c4b881ea71f
Apelsini <apelsin652@gmail.com>
2024-03-28 12:22:42 +0300

    results - adding location

--- polls/views.py
+++ polls/views.py
@@ -200,9 +200,8 @@ class ResultsView(generic.DetailView):
 @login_required(login_url=reverse_lazy('auth:login'))
 def results_urlentry(request, pk):
     urlentry = get_object_or_404(Urlentry, pk=pk)
-    objj = urlentry.leads_set.all()
     country_codes_dict = {}
-    for lead in objj:
+    for lead in urlentry.leads_set.all():
         if lead.follower_fromwhere not in country_codes_dict:
             try:
                country_code = get_country_code(lead.follower_fromwhere)
@@ -212,7 +211,7 @@ def results_urlentry(request, pk):
     return render(request, 'polls/results.html', {
         'urlentry': urlentry,
         'formset': country_codes_dict,
-        'count': objj,
+        'count': urlentry.leads_set.all(),
     })
 
 
